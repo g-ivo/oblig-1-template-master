748559a906425ba352dc6c1b4bc34e9b09a26060
JennyHov <s364545@oslomet.no>
2022-09-14 22:55:47 +0200

    Oppg 1,2,3 første commit. Ikke ferdig og får ikke bestått test.

--- README.md
+++ README.md
@@ -4,18 +4,21 @@ Denne oppgaven er en innlevering i Algoritmer og Datastrukturer.
 Oppgaven er levert av følgende studenter:
 * Jan Nicole Yao, S362049, s362049@oslomet.no
 * Joachim Ivo Di Mattia Martinsen s358995, s358995@oslomet.no
-* ...
+* Jenny Hovland s364545, s364545@oslomet.no
 
 # Arbeidsfordeling
 
 I oppgaven har vi hatt følgende arbeidsfordeling:
-* Jenny har hatt hovedansvar for oppgave 1, 2, 3.
-* Nicole har hatt hovedansvar for oppgave 4, 5, og 6.
-* Joachim har hatt hovedansvar for oppgave 7, 8 og 9. 
-* Vi har i fellesskap løst oppgave 10. 
+* Jenny har hatt hovedansvar for oppgave 1 og 2.
+* Nicole har hatt hovedansvar for oppgave 3 og 4.
+* Joachim har hatt hovedansvar for oppgave 5 og 6.
+* Noor har hatt hovedansvar for oppgave 7 og 8.
+* Vi har i fellesskap løst oppgave 9 og 10. 
 
 # Oppgavebeskrivelse
 
-I oppgave 1 så gikk vi frem ved å ...
+I oppgave 1 så gikk vi frem ved å først identifisere hvilken form for sortering som passet best til å løse oppgaven. 
+Fant ut av at bubble sort er metoden som brukes til å sammenligne to tall om gangen og bytter plass om den andre er større enn den første. 
+Deretter var det bare å lage for-løkker for å starte prosjektet. 
 
 I oppgave 2 så brukte vi en ... til å ...
--- src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -3,26 +3,97 @@ package no.oslomet.cs.algdat.Oblig1;
 ////// Løsningsforslag Oblig 1 ////////////////////////
 
 import java.lang.UnsupportedOperationException;
+import java.util.*;
 
 public class Oblig1 {
     private Oblig1() {}
 
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
-        throw new UnsupportedOperationException();
+        //Bubble sort for å plassere største verdi bakerst
+        int maksVerdi = 0;
+        if (a.length > 0) {
+            var count = 0;
+
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 1; j < i; j++) {
+                    if (a[j] > a[i]) {
+                        maksVerdi = a[j];
+                        a[j] = a[j - 1];
+                        a[j - 1] = maksVerdi;
+                        count++;
+                    }
+                }
+            }
+
+            System.out.printf("Count: %d", count);
+        } else {
+            throw new NoSuchElementException("Tabellen er tom!");
+        }
+        return maksVerdi;
     }
 
+        //returnere verdien igjen?
+
     public static int ombyttinger(int[] a) {
+        //Har bare skrevet kode for å finne maksverdi, eller skal man bruke fra forrige deloppgave?
+        int maksVerdi = a[0];
+
+        for( int i = 1; i < a.length; i++){
+            int verdi = a[i];
+            if(verdi > maksVerdi){
+                maksVerdi = verdi;
+            }
+        }
+
+        System.out.println(maksVerdi);
+
         throw new UnsupportedOperationException();
     }
 
     ///// Oppgave 2 //////////////////////////////////////
     public static int antallUlikeSortert(int[] a) {
+        //{3, 3, 4, 5, 5, 6, 7, 7, 7, 8};
+
+
+        //Finnes det en lettere metode uten ArrayList osv?
+        int antallForskjelligeVerdier = 0;
+
+        ArrayList<Integer> forskjelligeVerdier = new ArrayList<>();
+
+        for(int i = 0; i < a.length; i++){
+            if(!forskjelligeVerdier.contains(a[i])){
+                forskjelligeVerdier.add(a[i]); //Legger/fjerner verdier fra arrayet. Ikke bra.
+            }
+        }
+        if(forskjelligeVerdier.size()==1){
+            antallForskjelligeVerdier = 0;
+        } else {
+            antallForskjelligeVerdier = forskjelligeVerdier.size();
+        }
+        return antallForskjelligeVerdier;
+
         throw new UnsupportedOperationException();
     }
 
     ///// Oppgave 3 //////////////////////////////////////
     public static int antallUlikeUsortert(int[] a) {
+        int antallForskjelligeVerdier = 0;
+
+        ArrayList<Integer> forskjelligeVerdier = new ArrayList<>();
+
+        for(int i = 0; i < a.length; i++){
+            if(!forskjelligeVerdier.contains(a[i])){
+                forskjelligeVerdier.add(a[i]); //Legger/fjerner verdier fra arrayet. Ikke bra.
+            }
+        }
+        if(forskjelligeVerdier.size()==1){
+            antallForskjelligeVerdier = 0;
+        } else {
+            antallForskjelligeVerdier = forskjelligeVerdier.size();
+        }
+        return antallForskjelligeVerdier;
+
         throw new UnsupportedOperationException();
     }
 
